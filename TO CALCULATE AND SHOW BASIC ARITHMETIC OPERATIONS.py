53aded28fbcd3f391fca9640ef038466ce3da130
RisingBeastVector <saiayushmanp45@gmail.com>
2025-03-26 00:27:21 +0530

    Basic arithmetic operations

---
+++ TO CALCULATE AND SHOW BASIC ARITHMETIC OPERATIONS.py
@@ -0,0 +1,19 @@
+#PYTHON PROGRAM TO CALCULATE AND SHOW BASIC ARITMETIC OPERATIONS LIKE
+# +,-,*,%,**(POWER) AFTER ACCEPTING TWO NUMBERS.
+a = int(input('ENTER THE VALUE OF "a" : '))
+b = int(input('ENTER THE VALUE OF "b" : '))
+add = a+b
+sub = a-b
+mult = a*b
+div = a//b
+rem = a%b
+pow = a**b
+print()
+print("============================='OUTPUT'===============================")
+print()
+print('AFTER ADDITION : ', add)
+print('AFTER SUBTRACTION: ', sub)
+print('AFTER MULTIPLICATION : ', mult)
+print('AFTER DIVISION, THE QUOTIENT IS : ', div)
+print('AFTER DIVISION, THE REMAINDER IS : ', rem)
+print('AFTER POWERING BOTH THE VALUES: ', pow)
