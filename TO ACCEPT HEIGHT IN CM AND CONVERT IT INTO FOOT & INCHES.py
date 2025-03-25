128c3fed4e312d579e4560b26e76960c9776e106
RisingBeastVector <saiayushmanp45@gmail.com>
2025-03-25 23:48:46 +0530

    cm to foot & inch

---
+++ TO ACCEPT HEIGHT IN CM AND CONVERT IT INTO FOOT & INCHES.py
@@ -0,0 +1,6 @@
+#WRITE A PROGRAM TO ACCEPT HEIGHT IN CM AND CONVERT IT INTO FOOT & INCHES.
+cm = int(input('ENTER HEIGHT IN CENTIMETERS'))
+foot = cm/30.48
+inch = cm/2.54
+print('HEIGHT IN foot IS: ', foot)
+print('HEIGHT IN inches IS: ', inch)
